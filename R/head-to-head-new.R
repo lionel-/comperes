757970c2233f87a6b7974fcc4fcb15177775c4fd
evgeni.chasnovski <evgeni.chasnovski@gmail.com>
2018-02-06 21:20:15 +0200

    Update draft (use scalar fill in `h2h_mat()`; might be even faster then `fill` as list-argument to `h2h_long` in case of many players).

--- R/head-to-head-new.R
+++ R/head-to-head-new.R
@@ -11,28 +11,61 @@ h2h_long <- function(cr_data, ..., fill = list()) {
     add_class("h2h_long")
 }
 
+first_col_name <- function(tbl, silent = TRUE, target_name = "value") {
+  if (ncol(tbl) == 0) {
+    if (!silent) {
+      message("No ", target_name, " found. Using dummy vector.")
+    }
+
+    NULL
+  } else {
+    res <- colnames(tbl)[1]
+    if (!silent) {
+      message("Using ", res, " as ", target_name, ".")
+    }
+
+    res
+  }
+}
+
+miss_value <- function(type = NULL, fill = NULL) {
+  if (!identical(fill, NULL)) {
+    fill
+  } else {
+    if (identical(type, "list")) {
+      list(NULL)
+    } else {
+      NA
+    }
+  }
+}
+
 long_to_mat <- function(tbl, row_key, col_key, value = NULL,
-                        silent = TRUE) {
+                        fill = NULL, silent = TRUE) {
   row <- pull(tbl, !! enquo(row_key))
   col <- pull(tbl, !! enquo(col_key))
+
   if (identical(enquo(value), quo(NULL))) {
-    val <- tbl %>%
+    val_name <- tbl %>%
       select(- !! enquo(row_key), - !! enquo(col_key)) %>%
-      first_col(silent = silent, target_name = "value")
+      first_col_name(silent = silent)
+
+    if (identical(val_name, NULL)) {
+      mat_elem <- miss_value(NULL, fill)
+      val <- rep(mat_elem, nrow(tbl))
+    } else {
+      val <- tbl[[val_name]]
+      mat_elem <- miss_value(class(val), fill)
+    }
   } else {
     val <- pull(tbl, !! enquo(value))
-  }
-
-  if (is.list(val)) {
-    fill <- list(NULL)
-  } else {
-    fill <- NA
+    mat_elem <- miss_value(class(val), fill)
   }
 
   row_names <- levels2(row)
   col_names <- levels2(col)
   res <- matrix(
-    fill, nrow = length(row_names), ncol = length(col_names),
+    mat_elem, nrow = length(row_names), ncol = length(col_names),
     dimnames = list(row_names, col_names)
   )
   # For repairing in case `val` is list
@@ -44,31 +77,23 @@ long_to_mat <- function(tbl, row_key, col_key, value = NULL,
   res
 }
 
-as_h2h_mat <- function(tbl) {
+as_h2h_mat <- function(tbl, fill = NULL) {
   if (!inherits(tbl, "h2h_long")) {
     stop("`as_h2h_mat` must be applied only to `h2h_long` object.")
   }
 
   tbl %>%
-    long_to_mat(.data$player1, .data$player2,
+    long_to_mat(.data$player1, .data$player2, fill = fill,
                 silent = ncol(.) == 3) %>%
     add_class("h2h_mat")
 }
 
-h2h_mat <- function(cr_data, ...) {
+h2h_mat <- function(cr_data, ..., fill = NULL) {
   cr_data %>%
     h2h_long(...) %>%
-    as_h2h_mat()
-}
-
-fill_h2h_new <- function(x, fill) {
-  UseMethod("fill_h2h_new")
-}
-
-fill_h2h_new.default <- function(x, fill) {
-  stop("`fill_h2h` should be applied to either `h2h_long` or `h2h_mat` object.")
-}
-
-fill_h2h_new.h2h_long <- function(x, fill) {
-
+    # This might be even faster then `fill` as list-argument to `h2h_long`
+    # in case of many players
+    tidyr::drop_na() %>%
+    add_class_cond("h2h_long") %>%
+    as_h2h_mat(fill = fill)
 }
